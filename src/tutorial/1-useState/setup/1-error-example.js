69ec63e2305bfe13a303ecedd1a1152d99e00194
Nyota254 <knyota66@gmail.com>
2021-08-06 11:31:04 +0300

    why we use state

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
+//Why we need use state
+//initial idea is to change the title after button click
+//component doesnt rerender
+
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+
+  let title = "Just a title"
+
+  const changeTitle = () => {
+    title = "another title"
+    console.log(title)
+  }
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={changeTitle}>Change Title</button>
+    </div>
+  );
 };
 
 export default ErrorExample;
