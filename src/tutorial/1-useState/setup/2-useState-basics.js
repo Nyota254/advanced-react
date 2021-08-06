50f7503e5e35de5d2a39fb1fffe6cf8ae42a7648
Nyota254 <knyota66@gmail.com>
2021-08-06 12:40:15 +0300

    use State basics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,31 @@
 import React, { useState } from 'react';
+//useState is a Function
 
+//Hooks rules
+//Start with use
+//component must be uppercase where we use hooks
+//hooks must be in the function/component body
+//cannot be called conditionaly
+
+//State usage
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+
+  const [text,setText] = useState('just a heading')
+
+  const changeHeading = () => {
+    if(text === 'just a heading'){
+      setText('Another Heading');
+    } else {
+      setText('just a heading');
+    }
+  }
+
+  return(
+    <div>
+    <h1>{text}</h1>
+    <button type="button" className="btn" onClick={changeHeading}>Change Heading</button>
+    </div>
+  ); 
 };
 
 export default UseStateBasics;
