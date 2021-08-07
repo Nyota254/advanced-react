f7a8c16d439cc76921a176a2d93f39666ce97da6
Nyota254 <knyota66@gmail.com>
2021-08-07 14:05:38 +0300

    useState counter implementation returning function

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,40 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+
+  const [value,setValue] = useState(0)
+
+  const resetCounter = () => {
+    return setValue(0)
+  }
+
+//Complex counter function set state method return
+  const complexCounter = () => {
+    setTimeout(()=> {
+      setValue((prevState)=>{
+      return prevState + 1
+    });},2000);
+    
+  }
+
+  return (
+  <>
+  <section style={{margin:'4rem 0'}}>
+    <h2>Simple Counter</h2>
+    <h1>{value}</h1>
+    <button type="button" className="btn" onClick={()=>setValue(value - 1)}>- Decrease counter</button>
+    <button type="button" className="btn" onClick={resetCounter}>Reset</button>
+    <button type="button" className="btn" onClick={()=>setValue(value + 1)}>+ Increase counter</button>
+    </section>
+
+    <section style={{margin:'4rem 0'}}>
+    <h2>Complex Counter</h2>
+    <h1>{value}</h1>
+    <button type="button" className="btn" onClick={complexCounter}>+ Increase counter</button>
+    </section>
+  </>
+
+  );
 };
 
 export default UseStateCounter;
