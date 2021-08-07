274aed5e277d51bfc01d7c17669dca67aa2eb02f
Nyota254 <knyota66@gmail.com>
2021-08-07 11:27:46 +0300

    useState with objects

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person,setPerson]= useState({
+    name:'john',
+    secondName: 'wick',
+    profession: 'assasin',
+    status:'retired'
+  });
+
+  const changeStatus = () => {
+    if(person.status === 'retired'){
+      //Spread Operator assists in prefilling the values 
+      const newStatus = {...person,status:"Active"}
+      return setPerson(newStatus)
+    } else {
+      const newStatus = {...person,status:"retired"}
+      return setPerson(newStatus)
+    }
+    
+  }
+
+  return(
+  <>
+    <h2>Name: {person.name} {person.secondName}</h2>
+    <h2>Proffesion: {person.profession}</h2>
+    <h2>Status: {person.status}</h2>
+    <button type="button" className="btn" onClick={changeStatus}>Change Status</button>
+  </>
+  
+  );
 };
 
 export default UseStateObject;
