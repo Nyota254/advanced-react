0580f13202b695c125f5641f8f5e349e73db6df2
Nyota254 <knyota66@gmail.com>
2021-08-07 14:55:26 +0300

    useEffect basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,8 +2,22 @@ import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
+
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+
+  const [value,setValue] = useState(0)
+
+  useEffect(() => {
+    console.log('Use Effect called')
+    document.title = `Document Title ${value}`
+  });
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button type="button" className="btn" onClick={() => setValue(value+1)} >Click Me</button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
