8e599ba8d5caad18cc79940ef8e44fe188ee654a
Nyota254 <knyota66@gmail.com>
2021-08-08 17:36:34 +0300

    showing and hidding components

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,37 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+
+  const [show,setShow] = useState(false);
+
+  return (
+    <>
+      <button type="button" className="btn" onClick={()=>setShow(!show)}>Show Information</button>
+      {show && <HiddenSection/>}
+    </>
+    );
 };
+
+const HiddenSection = () => {
+
+  const [width,setWidth] = useState(window.innerWidth);
+
+  const changeWidth = () => {
+    setWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize',changeWidth)
+    return () => {
+    window.removeEventListener('resize',changeWidth)
+    }
+  }, []);
+
+  return(
+    <>
+      <h2 style={{margin:'2rem'}}>Window Width:{width}</h2>
+    </>
+  );
+}
 
 export default ShowHide;
