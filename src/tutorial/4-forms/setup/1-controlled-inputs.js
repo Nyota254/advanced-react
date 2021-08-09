7e27be7eb5715d820e8d66ba3a3fa0822e88a50e
Nyota254 <knyota66@gmail.com>
2021-08-09 10:40:16 +0300

    add name to list

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import uuid from 'react-uuid';
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +7,52 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName,setFirstName] = useState('');
+  const [email,setEmail] = useState('');
+  const [people,setPeople] = useState([]);
+  
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if(firstName && email){
+      const person = { id:uuid(),firstName,email };
+      console.log(person)
+      setPeople((people)=>{
+        return [...people,person];
+      })
+      setFirstName('');
+      setEmail('');
+    } else {
+      console.log('Kindly fill value')
+    }
+  }
+  
+  return(
+    <>
+    <article>
+      <form className="form" onSubmit={submitHandler}>
+        <div className="form-control">
+          <label htmlFor="firstName">Name:</label>
+          <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e)=>{setFirstName(e.target.value)}}/>
+        </div>
+        <div className="form-control">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+        </div>
+        <button type="submit">add person</button>
+      </form>
+    </article>
+    {
+    people.map((person)=>{
+      const {id,firstName,email} = person;
+        return(<div className="item" key={id}>
+          <h4>{firstName}</h4>
+          <p>{email}</p>
+        </div>)
+    })
+    }
+
+    </>
+  );
 };
 
 export default ControlledInputs;
