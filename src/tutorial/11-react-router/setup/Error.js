2216a8df71ef2182aab54ade01ed8d451d275096
Nyota254 <knyota66@gmail.com>
2021-08-10 13:46:30 +0300

    react-router

--- src/tutorial/11-react-router/setup/Error.js
+++ src/tutorial/11-react-router/setup/Error.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 const Error = () => {
   return (
     <div>
-      <h1>Error Page</h1>
+      <h1>OOPS!!Weve vered off too far</h1>
+      <Link to="/" className="btn">Back Home</Link>
     </div>
   );
 };
--- src/tutorial/11-react-router/setup/Home.js
+++ src/tutorial/11-react-router/setup/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <p>Hi there welcome to react routing</p>
     </div>
   );
 };
